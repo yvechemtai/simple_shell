f7dd31aac434b1d023adb1e5d16c523d7b7ae6ab
yvechemtai <yvechemtai@gmail.com>
2023-07-26 13:51:57 +0300

    Task 8

---
+++ Task-8.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * aux_help - Help information for the builtin help.
+ * Return: no return
+ */
+void aux_help(void)
+{
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
