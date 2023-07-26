6514afb73ac695e06a07bb67be13692bc539997f
yvechemtai <yvechemtai@gmail.com>
2023-07-26 13:51:07 +0300

    Task 7

---
+++ Task-7.c
@@ -0,0 +1,113 @@
+#include "main.h"
+
+/**
+ * exec_line - finds builtins and commands
+ *
+ * @datash: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(data_shell *datash)
+{
+	int (*builtin)(data_shell *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
+
+/**
+ * get_help - function that retrieves help messages according builtin
+ * @datash: data structure (args and input)
+ * Return: Return 0
+*/
+int get_help(data_shell *datash)
+{
+
+	if (datash->args[1] == 0)
+		aux_help_general();
+	else if (_strcmp(datash->args[1], "setenv") == 0)
+		aux_help_setenv();
+	else if (_strcmp(datash->args[1], "env") == 0)
+		aux_help_env();
+	else if (_strcmp(datash->args[1], "unsetenv") == 0)
+		aux_help_unsetenv();
+	else if (_strcmp(datash->args[1], "help") == 0)
+		aux_help();
+	else if (_strcmp(datash->args[1], "exit") == 0)
+		aux_help_exit();
+	else if (_strcmp(datash->args[1], "cd") == 0)
+		aux_help_cd();
+	else if (_strcmp(datash->args[1], "alias") == 0)
+		aux_help_alias();
+	else
+		write(STDERR_FILENO, datash->args[0],
+		      _strlen(datash->args[0]));
+
+	datash->status = 0;
+	return (1);
+}
+
+/**
+ * is_cdir - checks ":" if is in the current directory.
+ * @path: type char pointer char.
+ * @i: type int pointer of index.
+ * Return: 1 if the path is searchable in the cd, 0 otherwise.
+ */
+int is_cdir(char *path, int *i)
+{
+	if (path[*i] == ':')
+		return (1);
+
+	while (path[*i] != ':' && path[*i])
+	{
+		*i += 1;
+	}
+
+	if (path[*i])
+		*i += 1;
+
+	return (0);
+}
+
+/**
+ * check_error_cmd - verifies if user has permissions to access
+ *
+ * @dir: destination directory
+ * @datash: data structure
+ * Return: 1 if there is an error, 0 if not
+ */
+int check_error_cmd(char *dir, data_shell *datash)
+{
+	if (dir == NULL)
+	{
+		get_error(datash, 127);
+		return (1);
+	}
+
+	if (_strcmp(datash->args[0], dir) != 0)
+	{
+		if (access(dir, X_OK) == -1)
+		{
+			get_error(datash, 126);
+			free(dir);
+			return (1);
+		}
+		free(dir);
+	}
+	else
+	{
+		if (access(datash->args[0], X_OK) == -1)
+		{
+			get_error(datash, 126);
+			return (1);
+		}
+	}
+
+	return (0);
+}
