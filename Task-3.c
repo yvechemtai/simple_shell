3863bac26c98daa741433fc4104cee345f221a08
yvechemtai <yvechemtai@gmail.com>
2023-07-26 13:46:41 +0300

    Task 3

---
+++ Task-3.c
@@ -0,0 +1,105 @@
+#include "main.h"
+
+/**
+ * error_path_126 - error message for path and failure denied permission.
+ * @datash: data relevant (counter, arguments).
+ *
+ * Return: The error string.
+ */
+char *error_path_126(data_shell *datash)
+{
+	int length;
+	char *ver_str;
+	char *error;
+
+	ver_str = aux_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(ver_str);
+	length += _strlen(datash->args[0]) + 24;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(error);
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->args[0]);
+	_strcat(error, ": Permission denied\n");
+	_strcat(error, "\0");
+	free(ver_str);
+	return (error);
+}
+
+/**
+ * error_not_found - generic error message for command not found
+ * @datash: data relevant (counter, arguments)
+ * Return: Error message
+ */
+char *error_not_found(data_shell *datash)
+{
+	int length;
+	char *error;
+	char *ver_str;
+
+	ver_str = aux_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(ver_str);
+	length += _strlen(datash->args[0]) + 16;
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(error);
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->args[0]);
+	_strcat(error, ": not found\n");
+	_strcat(error, "\0");
+	free(ver_str);
+	return (error);
+}
+
+/**
+ * get_error - calls the error according the builtin, syntax or permission
+ * @datash: data structure that contains arguments
+ * @eval: error value
+ * Return: error
+ */
+int get_error(data_shell *datash, int eval)
+{
+	char *error;
+
+	switch (eval)
+	{
+	case -1:
+		error = error_env(datash);
+		break;
+	case 126:
+		error = error_path_126(datash);
+		break;
+	case 127:
+		error = error_not_found(datash);
+		break;
+	case 2:
+		if (_strcmp("exit", datash->args[0]) == 0)
+			error = error_exit_shell(datash);
+		else if (_strcmp("cd", datash->args[0]) == 0)
+			error = error_get_cd(datash);
+		break;
+	}
+
+	if (error)
+	{
+		write(STDERR_FILENO, error, _strlen(error));
+		free(error);
+	}
+
+	datash->status = eval;
+	return (eval);
+}
